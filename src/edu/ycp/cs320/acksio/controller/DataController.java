8e330784c2ac0fe1d8bd919473a89de3d4c434c3
AndrewGeorgiou <ageorgiou@ycp.edu>
2018-03-16 12:56:56 -0400

    test

--- src/edu/ycp/cs320/acksio/controller/DataController.java
+++ src/edu/ycp/cs320/acksio/controller/DataController.java
@@ -4,4 +4,5 @@ public interface DataController {
 	public void populate(String id);	// This method will make the database call: "select * from {insert table} where id = ?" and set id = id.
 	
 	public void save();					// This method will make the database call to update the database with the new information.
+	public void Queue();
 }
