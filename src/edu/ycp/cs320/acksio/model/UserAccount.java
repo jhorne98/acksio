afd65b230a3873d20b4ae0e60a13415697704515
AlaskaJay <thatthinker@gmail.com>
2018-03-16 13:50:37 -0400

    Added test cases to be implemented in time

--- src/edu/ycp/cs320/acksio/model/UserAccount.java
+++ src/edu/ycp/cs320/acksio/model/UserAccount.java
@@ -3,6 +3,10 @@ package edu.ycp.cs320.acksio.model;
 import edu.ycp.cs320.acksio.controller.DataController;
 
 public class UserAccount implements DataController{
+	//ATTRIBUTES
+	//TODO: Implement 
+	
+	//CONSTRUCTORS
 	public UserAccount() {
 		//TODO: Implement?
 	}
@@ -16,6 +20,8 @@ public class UserAccount implements DataController{
 		populate(id);
 	}
 
+	
+	//METHODS
 	@Override
 	public void populate(String id) {
 		// TODO Auto-generated method stub
@@ -32,6 +38,11 @@ public class UserAccount implements DataController{
 		//TODO: Implement 
 	}
 	
+	public boolean verifyLogin() {
+		//TODO: Implement
+		return false;
+	}
+	
 	public void logout() {
 		//TODO: Implement 
 	}
@@ -39,5 +50,13 @@ public class UserAccount implements DataController{
 	public boolean isLoggedIn() {
 		//TODO: Implement 
 		return false;
+	}
+	
+	public void edit(String username) {
+		//TODO: Implement 
+	}
+	
+	public void addVehicle(String username) {
+		//TODO: Implement 
 	}
 }
