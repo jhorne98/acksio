635f434dc2ff60c4e328324498b37231aa15fd68
Christian <christianawaldemar@gmail.com>
2018-03-19 10:42:08 -0400

    added attributes and constructor to courier

--- src/edu/ycp/cs320/acksio/model/Courier.java
+++ src/edu/ycp/cs320/acksio/model/Courier.java
@@ -2,6 +2,33 @@ package edu.ycp.cs320.acksio.model;
 
 public class Courier extends UserAccount{
 	
+	private int driverNumber;
+	private String name;
+	private boolean availability;
+	private int licenseID;
+	private String licenseExp;
+	private boolean insured;
+	private int[] insuranceCoverage;
+	private boolean tsaVerified;
+	private double payHistory;
+	private double payEstimate;
+	private double balance;
+	
+	
+	
+	
+	public Courier(int driverNumner, String name, boolean availability, int licenceID, String licenseExp, boolean insured, int[] insuranceCoverage, boolean tsaVeriflied, double payHistory, double payEstimate, double balance) {
+		this.driverNumber=driverNumber;
+		this.name=name;
+		this.availability=availability;
+		this.licenseID=licenseID;
+		this.insured=insured;
+		this.insuranceCoverage=insuranceCoverage;
+		this.tsaVerified=tsaVerified;
+		this.payHistory=payHistory;
+		this.payEstimate=payEstimate;
+		this.balance=balance;
+	}
 
 	@Override
 	public void populate(String id) {
