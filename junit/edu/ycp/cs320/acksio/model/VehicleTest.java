d8ab182e8ada05c3769d3347c2b097386efe7fe3
AlaskaJay <thatthinker@gmail.com>
2018-03-22 19:55:44 -0400

    Removed LoginTest and EmptyTest, Added VehicleTest
    no implementation yet

--- junit/edu/ycp/cs320/acksio/model/VehicleTest.java
+++ junit/edu/ycp/cs320/acksio/model/VehicleTest.java
@@ -1,14 +1,13 @@
 package edu.ycp.cs320.acksio.model;
 
 import static org.junit.Assert.*;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import edu.ycp.cs320.acksio.model.Numbers;
+import edu.ycp.cs320.acksio.model.Vehicle;
 
-public class EmptyTest {
+public class VehicleTest {
 	
 	
 	@Before
