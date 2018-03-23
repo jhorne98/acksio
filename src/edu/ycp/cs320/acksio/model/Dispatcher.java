e396a1868c7caa43ee7cd1736f73a8707d7cda47
Christian <christianawaldemar@gmail.com>
2018-03-23 13:23:10 -0400

    Fixed merge issues in dispatcher

--- src/edu/ycp/cs320/acksio/model/Dispatcher.java
+++ src/edu/ycp/cs320/acksio/model/Dispatcher.java
@@ -1,17 +1,20 @@
-<<<<<<< HEAD
+
 package edu.ycp.cs320.acksio.model;
 
 public class Dispatcher extends UserAccount{
 	private VehicleType vehicleType;
 	private Boolean tsaCert;
+	private Job testJob; //will go to database when implemented 
+	private String address;
+	private String name;
+	private int phone;
 	
-	public Dispatcher() {
-		
-	}
-	
-	public Dispatcher(VehicleType vehicleType, Boolean tsaCert) {
+	public Dispatcher(VehicleType vehicleType, Boolean tsaCert, String address, String name, int phone) {
 		this.vehicleType = vehicleType;
 		this.tsaCert = tsaCert;
+		this.address = address;
+		this.phone = phone;
+		this.name = name; 
 	}
 	
 	public VehicleType getVehicleType() {
@@ -20,7 +23,6 @@ public class Dispatcher extends UserAccount{
 	public void Queue() {
 		//TODO: Implement
 		Job testJob = new Job("118 oak drive", vehicleType.CAR, true, "Don Hake", 7175555555L, 64.9, 53.7, 53.7, 1430, 1730);
-		
 	}
   
 	public Boolean getTsaCert() {
@@ -40,64 +42,3 @@ public class Dispatcher extends UserAccount{
 	}
 
 }
-=======
-package edu.ycp.cs320.acksio.model;
-
-public class Dispatcher extends UserAccount{
-	private String vehicleType;
-	private Boolean tsaCert;
-	private Job testJob; //will go to database when implemented 
-	private String address;
-	private String name;
-	private int phone;
-	
-	public Dispatcher() {
-		
-	}
-	
-	public Dispatcher(String vehicleType, Boolean tsaCert, String address, String name, int phone) {
-		this.vehicleType = vehicleType;
-		this.tsaCert = tsaCert;
-		this.address = address;
-		this.phone = phone;
-		this.name = name; 
-	}
-	
-	public String getVehicleType() {
-		return vehicleType;
-	}
-	public void Queue() {
-		//TODO: Implement
-		testJob = new Job();
-		testJob.setActualTime(10); //Fixed number for testing
-		testJob.setCourierPaid(tsaCert);
-		testJob.setDeststinationAddress(address);
-		testJob.setDistanceMi(10); //Fixed number for testing
-		testJob.setDropOffTime(10); //Fixed number for testing
-		testJob.setPayActualForJob(10); //Fixed number for testing
-		testJob.setPayEstimateForJob(10);//Fixed number for testing
-		testJob.setPickUpTime(10); //Fixed number for testing
-		testJob.setRecipentName(name);
-		testJob.setRecipentPhone(phone);
-		testJob.setTsaVerified(tsaCert);
-		testJob.setVehicleType(vehicleType);
-	}
-  
-	public Boolean getTsaCert() {
-		return tsaCert;
-	}
-	
-	@Override
-	public void populate(String id) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void save() {
-		// TODO Auto-generated method stub
-		
-	}
-
-}
->>>>>>> refs/heads/master
