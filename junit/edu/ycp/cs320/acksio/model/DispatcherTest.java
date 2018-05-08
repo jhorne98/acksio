87af9d64949a82cbc67372e2378b9e3b21fd9d1f
Joel Horne <jhorne@ycp.edu>
2018-05-08 19:16:23 -0400

    Added get and set cases to DispatcherTest

--- junit/edu/ycp/cs320/acksio/model/DispatcherTest.java
+++ junit/edu/ycp/cs320/acksio/model/DispatcherTest.java
@@ -7,26 +7,32 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class DispatcherTest {
-	
+	private Dispatcher model;
 	
 	@Before
 	public void setUp() {
+		model = new Dispatcher();
 		
+		model.setAddress("11313 Address Rd.");
+		model.setDispatcherID(2);
+		model.setPayment(2.0);
+		model.setPhone("7177495979");
 	}
 	
-	@Test 
-	public void testGets() {
-		
-	}
-	
+	@Test
 	public void testSets() {
-		
+		assertEquals("11313 Address Rd.", model.getAddress());
+		assertEquals(2, model.getDispatcherID());
+		assertEquals(2.0, model.getPayment(), 0.01);
+		assertEquals("7177495979", model.getPhone());
 	}
 	
+	@Test
 	public void testQueue() {
 		
 	}
 	
+	@Test
 	public void testPay() {
 		
 	}
